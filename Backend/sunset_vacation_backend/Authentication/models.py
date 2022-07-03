0231d16f48afc67ba4f7eeffabb503bcc6451f50
sumaiyaazad <azadsumaiya00@gmail.com>
2022-07-03 12:45:21 +0600

    added user model

--- Backend/sunset_vacation_backend/Authentication/models.py
+++ Backend/sunset_vacation_backend/Authentication/models.py
@@ -1,3 +1,59 @@
 from django.db import models
 
+
 # Create your models here.
+
+class User(models.Model):
+    user_id = models.AutoField(
+        primary_key=True
+    )
+
+    name = models.CharField(
+        max_length=200,
+        default=None,
+        blank=False,
+        null=True
+    )
+
+    email = models.EmailField(
+        max_length=50,
+        default=None,
+        blank=False,
+        null=True,
+        unique=True
+    )
+
+    phone_no = models.CharField(
+        max_length=20,
+        default=None,
+        blank=False,
+        null=True
+    )
+
+    password = models.CharField(
+        max_length=500,
+        default=None,
+        blank=False,
+        null=True
+    )
+
+    address = models.CharField(
+        max_length=200,
+        default=None,
+        blank=False,
+        null=True
+    )
+
+    country = models.CharField(
+        max_length=20,
+        default=None,
+        blank=False,
+        null=True
+    )
+
+    city = models.CharField(
+        max_length=20,
+        default=None,
+        blank=False,
+        null=True
+    )
