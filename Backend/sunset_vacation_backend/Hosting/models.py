acbe1c2315b9cf7f85e442ef808b585a3f2e6be4
umama5177 <umamarahman5177@gmail.com>
2022-07-04 13:39:49 +0600

    hosting database created

--- Backend/sunset_vacation_backend/Hosting/models.py
+++ Backend/sunset_vacation_backend/Hosting/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from Authentication.models import User
 
 # Create your models here.
 
@@ -7,6 +8,8 @@ class Property(models.Model):
     propertyID=models.AutoField(
         primary_key=True
     )
+
+    owner_id=models.ForeignKey(User,on_delete=models.CASCADE,default=None)
 
     title=models.CharField(
         max_length=100,
@@ -74,6 +77,151 @@ class Property(models.Model):
         blank=False,
         null=True
     )
+
+
+
+
+class Catagory(models.Model):
+    catagory_id=models.AutoField(
+        primary_key=True
+    )
+    description=models.CharField(
+        max_length=100,
+        default=None,
+        blank=False,
+        null=True
+    )
+
+class Offer(models.Model):
+    offer_id=models.AutoField(
+        primary_key=True
+    )
+    startDate=models.DateTimeField(
+        default=None
+    )
+    endDate=models.DateTimeField(
+        default=None
+    )
+    amount=models.FloatField(
+        default=None
+    )
+    propertyID=models.ForeignKey(
+        Property,
+        on_delete=models.CASCADE,
+        null=False
+    )
+class House_Rules(models.Model):
+    rule_id=models.AutoField(primary_key=True)
+    propertyID=models.ForeignKey(Property,on_delete=models.CASCADE)
+    do_dont_flag=models.IntegerField(default=None,null=True)
+    rule=models.CharField(
+        max_length=100,
+        default=None,
+        blank=False,
+        null=True
+    )
+
+class FAQ(models.Model):
+    faq_id=models.AutoField(primary_key=True)
+    propertyID=models.ForeignKey(Property,on_delete=models.CASCADE)
+    question=models.CharField(
+        max_length=100,
+        default=None,
+        blank=False,
+        null=True
+    )
+    answer=models.CharField(
+        max_length=100,
+        default=None,
+        blank=False,
+        null=True
+    )
+class Facility(models.Model):
+    facility_name=models.CharField(
+        primary_key=True, 
+        max_length=100,
+        default=None,
+        blank=False)
+    catagory=models.CharField(
+        max_length=100,
+        default=None,
+        blank=False,
+        null=True
+    )
+class PropertyFacilities(models.Model):
+    propertyID=models.ForeignKey(Property,on_delete=models.CASCADE)
+    facility_name=models.ForeignKey(Facility,on_delete=models.CASCADE)
+    description=models.CharField(
+        max_length=100,
+        default=None,
+        blank=False,
+        null=True
+    )
+class Ratings(models.Model):
+    user_id=models.ForeignKey(User,on_delete=models.CASCADE)
+    propertyID=models.ForeignKey(Property,on_delete=models.CASCADE)
+    rating=models.FloatField(default=None,null=True)
+
+class Reviews(models.Model):
+    user_id=models.ForeignKey(User,on_delete=models.CASCADE)
+    propertyID=models.ForeignKey(Property,on_delete=models.CASCADE)
+    review=models.CharField(
+        max_length=100,
+        default=None,
+        blank=False,
+        null=True
+    )
+class Notification(models.Model):
+    user_id=models.ForeignKey(User,on_delete=models.CASCADE)
+    text=models.CharField(
+        max_length=100,
+        default=None,
+        blank=False,
+        null=True
+    )
+    link=models.CharField(
+        max_length=100,
+        default=None,
+        blank=False,
+        null=True
+    )
+    time=models.DateTimeField(
+        default=None,
+        null=True
+    )
+    marked=models.BooleanField(default=False)
+
+class Messaging(models.Model):
+    msg_id=models.AutoField(primary_key=True)
+    sender_id=models.ForeignKey(User,on_delete=models.CASCADE,related_name="sender")
+    receiver_id=models.ForeignKey(User,on_delete=models.CASCADE,related_name="receiver")
+    message=models.CharField(
+        max_length=100,
+        default=None,
+        blank=False,
+        null=True
+    )
+    time=models.DateTimeField(
+        default=None,
+        null=True
+    )
+
+class GiftCard(models.Model):
+    giftcard_id=models.AutoField(primary_key=True)
+    propertyID=models.ForeignKey(Property,on_delete=models.CASCADE)
+    type=models.CharField(
+        max_length=100,
+        default=None,
+        blank=False,
+        null=True
+    )
+    discount=models.FloatField(default=None,null=True)
+    expiry_date=models.DateTimeField(default=None,null=True)
+
+class UserGiftCardList(models.Model):
+    user_id=models.ForeignKey(User,on_delete=models.CASCADE)
+    giftcard_id=models.ForeignKey(GiftCard,on_delete=models.CASCADE)
+    used_flag=models.BooleanField(default=False)
 
 
 
--- Backend/sunset_vacation_backend/Hosting/serializer.py
+++ Backend/sunset_vacation_backend/Hosting/serializer.py
@@ -1,4 +1,4 @@
-from .models import Property
+from .models import *
 from rest_framework import serializers
 
 class PropertySerializer(serializers.ModelSerializer):
@@ -16,7 +16,7 @@ class PropertySerializer(serializers.ModelSerializer):
     checkInTime=serializers.DateTimeField(required=False)
     checkOutTime=serializers.DateTimeField(required=False)
     maxDaysRefund=serializers.IntegerField(required=False)
-
+    owner_id=serializers.PrimaryKeyRelatedField(read_only=True)
 
     # def create(self,data):
     #     return Property.objects.create(
@@ -39,3 +39,144 @@ class PropertySerializer(serializers.ModelSerializer):
     class Meta:
         model=Property
         fields = '__all__'
+
+
+class CatagorySerializer(serializers.ModelSerializer):
+    description=serializers.CharField(max_length=100,required=False)
+
+    class Meta:
+        model=Catagory
+        fields = '__all__'
+
+class OfferSerializer(serializers.ModelSerializer):
+    startDate=serializers.DateTimeField(
+        required=False
+    )
+    endDate=serializers.DateTimeField(
+        required=False
+    )
+    amount=serializers.FloatField(
+        required=False
+    )
+    propertyID=serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        model=Offer
+        fields = '__all__'
+
+class House_RulesSerializer(serializers.ModelSerializer):
+    propertyID=serializers.PrimaryKeyRelatedField(read_only=True)
+    do_dont_flag=serializers.IntegerField(required=False)
+    rule=serializers.CharField(max_length=100,required=False)
+
+    class Meta:
+        model=House_Rules
+        fields = '__all__'
+
+class FAQSerializer(serializers.ModelSerializer):
+    propertyID=serializers.PrimaryKeyRelatedField(read_only=True)
+    question=serializers.CharField(max_length=100,required=False)
+    answer=serializers.CharField(max_length=100,required=False)
+
+    class Meta:
+        model=FAQ
+        fields = '__all__'
+
+class FacilitySerializer(serializers.ModelSerializer):
+    catagory=serializers.CharField(
+        max_length=100,
+       required=False
+    )
+    class Meta:
+        model=Facility
+        fields = '__all__'
+
+class PropertyFacilitiesSerializer(serializers.ModelSerializer):
+    propertyID=serializers.PrimaryKeyRelatedField(read_only=True)
+    facility_name=serializers.PrimaryKeyRelatedField(read_only=True)
+    description=serializers.CharField(
+        max_length=100,
+        required=False
+    )
+
+    class Meta:
+        model=PropertyFacilities
+        fields = '__all__'
+
+class RatingsSerializer(serializers.ModelSerializer):
+    user_id=serializers.PrimaryKeyRelatedField(read_only=True)
+    propertyID=serializers.PrimaryKeyRelatedField(read_only=True)
+    rating=serializers.FloatField(required=False)
+
+    class Meta:
+        model=Ratings
+        fields = '__all__'
+
+class ReviewsSerializer(serializers.ModelSerializer):
+    user_id=serializers.PrimaryKeyRelatedField(read_only=True)
+    propertyID=serializers.PrimaryKeyRelatedField(read_only=True)
+    review=serializers.CharField(max_length=100,required=False)
+
+    class Meta:
+        model=Reviews
+        fields = '__all__'
+
+class NotificationSerializer(serializers.ModelSerializer):
+    user_id=serializers.PrimaryKeyRelatedField(read_only=True)
+    text=serializers.CharField(
+        max_length=100,
+       required=False
+    )
+    link=serializers.CharField(
+        max_length=100,
+        required=False
+    )
+    time=serializers.DateTimeField(
+       required=False
+    )
+    marked=serializers.BooleanField(required=False)
+
+    class Meta:
+        model=Notification
+        fields = '__all__'
+
+class MessagingSerializer(serializers.ModelSerializer):
+    sender_id=serializers.PrimaryKeyRelatedField(read_only=True)
+    receiver_id=serializers.PrimaryKeyRelatedField(read_only=True)
+    message=serializers.CharField(
+        max_length=100,
+        required=False
+    )
+    time=serializers.DateTimeField(
+        required=False
+    )
+    class Meta:
+        model=Messaging
+        fields = '__all__'
+
+class GiftCardSerializer(serializers.ModelSerializer):
+    propertyID=serializers.PrimaryKeyRelatedField(read_only=True)
+    type=serializers.CharField(
+        max_length=100,
+        required=False
+    )
+    discount=serializers.FloatField(required=False)
+    expiry_date=serializers.DateTimeField(required=False)
+
+    class Meta:
+        model=GiftCard
+        fields = '__all__'
+
+class UserGiftCardListSerializer(serializers.ModelSerializer):
+    user_id=serializers.PrimaryKeyRelatedField(read_only=True)
+    giftcard_id=serializers.PrimaryKeyRelatedField(read_only=True)
+    used_flag=serializers.BooleanField(required=False)
+
+    class Meta:
+        model=UserGiftCardList
+        fields = '__all__'
+
+
+
+
+    
