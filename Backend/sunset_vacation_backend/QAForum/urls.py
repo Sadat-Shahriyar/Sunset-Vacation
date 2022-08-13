bbc1999b0e73fcd160ce28defe59af9c0b132690
Sadat-Shahriyar <57701103+Sadat-Shahriyar@users.noreply.github.com>
2022-08-13 14:20:52 +0600

    qa forum models

--- Backend/sunset_vacation_backend/QAForum/urls.py
+++ Backend/sunset_vacation_backend/QAForum/urls.py
@@ -3,8 +3,5 @@ from . import views
 
 
 urlpatterns = [
-    #path('<int:user_id>/', views.getProperties),
-    path('propertydetails/<int:property_id>/',views.getPropertyDetails),
-    path('reserve/', views.reserve),
-    path('checkAvailabilityOfDate/', views.checkAvailabilityOfDate),
+    path('getAllQuestions/',views.getAllQuestions),
 ]
