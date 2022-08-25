3cd405661afdffd5d31910be55d6107075e0b989
sumaiyaazad <azadsumaiya00@gmail.com>
2022-08-25 17:11:52 +0600

    mark message connected with frontend

--- Frontend/sunset_vacation_frontend/src/Components/Messaging/Inbox.js
+++ Frontend/sunset_vacation_frontend/src/Components/Messaging/Inbox.js
@@ -11,70 +11,68 @@ import Paper from '@mui/material/Paper';
 import { Button, Grid } from '@mui/material';
 import ReplyMessage from './ReplyMessage';
 import {fontWeight} from "@mui/system";
+function ViewMessages(props){
 
+    const handleReply = async(message)=>{
+
+        const requestOptions = {
+            method: 'PATCH',
+            headers: { 'Content-Type': 'application/json',
+                'Authorization': `Bearer ${props.token}` },
+        };
+
+        fetch('http://localhost:8000/message/markMessage/'+message.msg_id, requestOptions)
+            .then(response => response.json())
+            .then(response => {
+                props.setMessageToReply(message);
+                props.setReply(true);
+            }).catch((err) => {
+            console.log(err);
+          });
+    }
+
+    // const handleReply = (message) => {
+    //     props.setMessageToReply(message);
+    //     props.setReply(true);
+    // }
+
+    return (
+        <Grid container sx={{maxWidth:900, ml:40, mt:5}}>
+            <Grid item xs={12}>
+                <TableContainer component={Paper}>
+                    <Table sx={{ minWidth: 650,  }} aria-label="simple table">
+                        <TableHead sx={{background:'pink', }}>
+                            <TableRow >
+                                <TableCell align="center" colSpan={3} sx={{fontFamily: "Lucida Handwriting"}}>Messages</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {props.messages.map((message) => {
+                                return(
+                                    <TableRow
+                                        key={message.msg_id}
+                                        hover
+                                        sx={{ '&:last-child td, &:last-child th': { border: 0 }, paddingRight:5}}
+                                        onClick={() => {handleReply(message)}}
+                                    >
+                                        <TableCell  sx={{ paddingLeft:5, fontWeight:"bold", fontFamily: "Lucida Handwriting", fontSize:15}}>{message.name}</TableCell>
+                                        <TableCell  sx={{ paddingLeft:2, fontFamily: "Lucida Handwriting", fontSize:13,  fontWeight: message.marked  ? "light": "bold"}} >{message.sender_name} {message.message}</TableCell>
+                                    </TableRow>
+                                );
+                            })}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Grid>
+        </Grid>
+    );
+}
 
 export default function Inbox(props){
 
     const [messages, setMessages] = React.useState([])
     const [messageToReply, setMessageToReply] = React.useState(null);
     const [reply, setReply] = React.useState(false);
-    function ViewMessages(props){
-
-        const handleReply = async(message)=>{
-            try{
-                let response = await axios_api.patch('message/mark/'+message.msg_id, {
-                    headers: {
-                        "Content-Type": "application/json",
-                        'Authorization' : `Bearer ${props.token}`
-                    }
-                })
-                if(response.status === 200){
-                    setMessages(response.data.messages);
-                }
-            }
-            catch(error){
-                alert(error.message);
-            }
-        }
-
-        // const handleReply = (message) => {
-        //
-        //     props.setMessageToReply(message);
-        //     props.setReply(true);
-        // }
-
-        return (
-            <Grid container sx={{maxWidth:900, ml:40, mt:5}}>
-                <Grid item xs={12}>
-                    <TableContainer component={Paper}>
-                        <Table sx={{ minWidth: 650,  }} aria-label="simple table">
-                            <TableHead sx={{background:'pink', }}>
-                                <TableRow >
-                                    <TableCell align="center" colSpan={3} sx={{fontFamily: "Lucida Handwriting"}}>Messages</TableCell>
-                                </TableRow>
-                            </TableHead>
-                            <TableBody>
-                                {props.messages.map((message) => {
-                                    return(
-                                        <TableRow
-                                            hover
-                                            key={message.message_id}
-                                            sx={{ '&:last-child td, &:last-child th': { border: 0 }, paddingRight:5}}
-                                            onClick={() => {handleReply(message)}}
-                                        >
-                                            <TableCell  sx={{ paddingLeft:5, fontWeight:"bold", fontFamily: "Lucida Handwriting", fontSize:15}}>{message.name}</TableCell>
-                                            <TableCell  sx={{ paddingLeft:5, fontWeight:"bold", fontFamily: "Lucida Handwriting", fontSize:15}}>{message.msg_id} {String(message.marked)}</TableCell>
-                                            <TableCell  sx={{ paddingLeft:2, fontFamily: "Lucida Handwriting", fontSize:13,  fontWeight: message.marked  ? "light": "bold"}} >{message.sender_name} {message.message}</TableCell>
-                                        </TableRow>
-                                    );
-                                })}
-                            </TableBody>
-                        </Table>
-                    </TableContainer>
-                </Grid>
-            </Grid>
-        );
-    }
     const fetchMessages = async()=>{
         try{
             let response = await axios_api.get('message/getMessages/', {
@@ -114,6 +112,7 @@ export default function Inbox(props){
                 <InboxNavbar />
                 <ViewMessages
                     messages = {messages}
+                    token = {props.token}
                     setMessageToReply = {(val) => {setMessageToReply(val)}}
                     setReply = {(val) => {setReply(val)}}
                 />
