3f137c8799bf816ae79e11f89651a43468c0d314
sumaiyaazad <azadsumaiya00@gmail.com>
2022-08-26 10:36:31 +0600

    sending message from frontend not working yet

--- Frontend/sunset_vacation_frontend/src/Components/Messaging/ReplyMessage.js
+++ Frontend/sunset_vacation_frontend/src/Components/Messaging/ReplyMessage.js
@@ -12,7 +12,7 @@ import CardContent from "@mui/material/CardContent";
 
 export default function ReplyMessage(props) {
     const [message, setMessage] = React.useState("");
-    const [userId, setUserId] =  React.useState("");
+    const [userId, setUserId] = React.useState("");
     const [friendId, setFriendId] = React.useState("");
     const [userName, setUserName] = React.useState("");
     const [friendName, setFriendName] = React.useState("");
@@ -32,16 +32,16 @@ export default function ReplyMessage(props) {
                 setFriendId(props.messageToReply.sender_id);
                 setUserId(props.messageToReply.receiver_id);
                 localFriendId = props.messageToReply.sender_id;
-            }else{
+            } else {
                 setFriendId(props.messageToReply.receiver_id);
                 setUserId(props.messageToReply.sender_id);
                 localFriendId = props.messageToReply.receiver_id;
             }
-            console.log("userId: "+userId);
-            console.log("friendId: "+friendId);
-            console.log("localfriendId: "+localFriendId);
+            console.log("userId: " + userId);
+            console.log("friendId: " + friendId);
+            console.log("localfriendId: " + localFriendId);
 
-            let messageResponse = await axios_api.get('message/getMessages/'+localFriendId,  {
+            let messageResponse = await axios_api.get('message/getMessages/' + localFriendId, {
                 headers: {
                     "Content-Type": "application/json",
                     'Authorization': `Bearer ${props.token}`
@@ -51,7 +51,7 @@ export default function ReplyMessage(props) {
             if (messageResponse.status === 200) {
                 console.log(messageResponse.data);
                 setFriendName(messageResponse.data.friendName);
-                setUserName(messageResponse.data.userName+"(You)");
+                setUserName(messageResponse.data.userName + "(You)");
                 setMessageThread(messageResponse.data.messages);
                 // props.setReply(false);
                 // props.navigate('/inbox');
@@ -66,43 +66,37 @@ export default function ReplyMessage(props) {
         fetchMessages()
     }, [])
 
-    function MainView(props) {
-        const handleSubmit2 = async() =>{
+    const [replyMessage, setReplyMessage] = React.useState("");
 
+    function MainView(props) {
+        function changeReplyMessage(event) {
+            setReplyMessage(event.target.value);
         }
 
         const handleSubmit = async () => {
-            console.log(props.message);
             try {
-                let response = await axios_api.get("users/verify/",
-                    {
-                        headers: {
-                            'Authorization': `Bearer ${props.token}`
-                        }
-                    })
+                console.log("hnadle submit friend id: " + friendId);
+                console.log("hnadle submit reply message: " + replyMessage);
+                if (replyMessage.length > 200) {
+                    alert("The message length exceeded max length 200");
+                    return;
+                }
+                let body = {
+                    receiver_id: friendId,
+                    message: replyMessage
+                }
 
-                console.log(response);
-                console.log("hello")
-                console.log(props.isLoggedin)
-                if (props.isLoggedin && response.data.valid) {
-                    console.log("hello2")
-                    let body = {
-                        receiver_id: props.messageToReply.sender_id,
-                        message: props.message
+                let messageResponse = await axios_api.post('message/sendMessage/', body, {
+                    headers: {
+                        "Content-Type": "application/json",
+                        'Authorization': `Bearer ${props.token}`
                     }
+                })
 
-                    let messageResponse = await axios_api.post('message/sendmessage/', body, {
-                        headers: {
-                            "Content-Type": "application/json",
-                            'Authorization': `Bearer ${props.token}`
-                        }
-                    })
-
-                    if (messageResponse.status === 200) {
-                        props.setReply(false);
-                        alert("The message has been sent");
-                        // props.navigate('/inbox');
-                    }
+                if (messageResponse.status === 200) {
+                    props.setReply(false);
+                    alert("The message has been sent");
+                    // props.navigate('/inbox');
                 }
 
                 console.log("hell3")
@@ -113,43 +107,65 @@ export default function ReplyMessage(props) {
 
 
         return (
-            <Grid container sx={{maxWidth: 1400,  ml:10, mt:5}}>
+            <Grid container sx={{maxWidth: 1400, ml: 10, mt: 5}}>
                 <Grid item xs={2}></Grid>
                 <Grid item xs={6}>
                     {/*<TextField variant='standard' value={props.message} onChange={(event) => {*/}
                     {/*    props.setMessage(event.target.value)*/}
                     {/*}}/>*/}
-                    <div style={{minWidth: 900, maxHeight: 450, minHeight:450, overflow: "hidden", overflowY: 'scroll'}}>
-                    {messageThread.map((message,index)=>{
-                        return(
-                            <Card sx={{minWidth: 900, mb:1, backgroundColor:message.sender_id === userId? 'pink': 'white'}} key={index}>
-                        <CardContent>
-                            <Typography sx={{ paddingX:1, fontWeight:"bold", fontFamily: "Lucida Handwriting", fontSize:16}}>
-                                { message.sender_id === userId ? userName : friendName}
-                                <Typography variant="subtitle" sx={{ padding:1, fontFamily: "Lucida Handwriting", fontSize:10, mb: 1.5}}  color="text.secondary">
-                                    {message.time}
-                                </Typography>
-                            </Typography>
+                    <div style={{
+                        minWidth: 900,
+                        maxHeight: 450,
+                        minHeight: 450,
+                        overflow: "hidden",
+                        overflowY: 'scroll'
+                    }}>
+                        {messageThread.map((message, index) => {
+                            return (
+                                <Card sx={{
+                                    minWidth: 900,
+                                    mb: 1,
+                                    backgroundColor: message.sender_id === userId ? 'pink' : 'white'
+                                }} key={index}>
+                                    <CardContent>
+                                        <Typography sx={{
+                                            paddingX: 1,
+                                            fontWeight: "bold",
+                                            fontFamily: "Lucida Handwriting",
+                                            fontSize: 16
+                                        }}>
+                                            {message.sender_id === userId ? userName : friendName}
+                                            <Typography variant="subtitle" sx={{
+                                                padding: 1,
+                                                fontFamily: "Lucida Handwriting",
+                                                fontSize: 10,
+                                                mb: 1.5
+                                            }} color="text.secondary">
+                                                {message.time}
+                                            </Typography>
+                                        </Typography>
 
-                            <Typography variant="body2" sx={{ padding:1, fontFamily: "Lucida Handwriting", fontSize:13}}>
-                                {message.message}
-                            </Typography>
+                                        <Typography variant="body2"
+                                                    sx={{padding: 1, fontFamily: "Lucida Handwriting", fontSize: 13}}>
+                                            {message.message}
+                                        </Typography>
 
-                        </CardContent>
-                    </Card>
-                    )})}
-                </div>
+                                    </CardContent>
+                                </Card>
+                            )
+                        })}
+                    </div>
                     <div style={{
                         display: 'flex',
                         alignItems: 'center'
                     }}>
-                    <TextField
-                        sx={{minWidth: 770, mt:2, mr:2}}
-                        id="outlined-textarea"
-                        rows={2}
-                        placeholder="Send Reply"
-                        onChange={handleSubmit2}/>
-                        <Button variant="contained" sx={{minWidth: 100, mt:1}}>Send </Button>
+                        <TextField
+                            sx={{minWidth: 770, mt: 2, mr: 2}}
+                            id="outlined-textarea"
+                            rows={2}
+                            placeholder="Send Reply"
+                            onChange={changeReplyMessage}/>
+                        <Button variant="contained" sx={{minWidth: 100, mt: 1}} onClick={handleSubmit}>Send</Button>
                     </div>
                 </Grid>
                 <Grid item xs={2}>
@@ -163,7 +179,9 @@ export default function ReplyMessage(props) {
 
     return (
         <div>
-            <InboxNavbar reply={props.reply} setReply = {(val) => {props.setReply(val)}}/>
+            <InboxNavbar reply={props.reply} setReply={(val) => {
+                props.setReply(val)
+            }}/>
             <MainView
                 message={message}
                 setMessage={(val) => {
